d7fd77d2541edc493c666f269cc4e8bcc90bb599
DenysReryt <denysreryt28@gmail.com>
2023-03-07 15:46:53 +0200

    Some editing for right working of CI

--- stocks/__init__.py
+++ stocks/__init__.py
@@ -23,5 +23,5 @@ def index():
     return 'Hello world!'
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='127.0.0.1')
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000, host='127.0.0.1')
