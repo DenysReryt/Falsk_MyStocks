887b5a3634f7313e043fa40f1c4e8c23ccd81f5c
DenysReryt <denysreryt28@gmail.com>
2023-04-18 18:58:44 +0300

    work with user_crud_test

--- tests/users/user_crud_test.py
+++ tests/users/user_crud_test.py
@@ -47,6 +47,20 @@ class TestUserCrud(unittest.TestCase):
         self.assertEqual(user, self.user1)
 
     @patch('stocks.service.user_crud.db')
+    def test_create_user(self, mock_db):
+        mock_db.session.add.return_value = MagicMock()
+        mock_db.session.commit.return_value = None
+
+        user = create_user('Bob', 'Smith', '5555555555')
+
+        mock_db.session.add.assert_called_once()
+        mock_db.session.commit.assert_called_once()
+        self.assertEqual(user.first_name, 'Bob')
+        self.assertEqual(user.last_name, 'Smith')
+        self.assertEqual(user.phone, '5555555555')
+
+
+    @patch('stocks.service.user_crud.db')
     def test_update_user(self, mock_db):
         # Create a mock user object to be returned by the query
         mock_user = MagicMock()
@@ -70,39 +84,25 @@ class TestUserCrud(unittest.TestCase):
 
         # Assert that the updated user object was returned
         self.assertEqual(updated_user, mock_user)
-    # def test_create_user(self, mock_db):
-    #     mock_db.session.add.return_value = MagicMock()
-    #     mock_db.session.commit.return_value = None
+    # def test_update_user(self, mock_session):
+    #     # Create a mock User object
+    #     mock_user = User(id=1, first_name='John', last_name='Doe', phone='1234567890')
     #
-    #     user = create_user('Bob', 'Smith', '5555555555')
+    #     # Set up the mock session to return the mock User object when queried
+    #     mock_session.query.return_value.filter_by.return_value.first.return_value = mock_user
     #
-    #     mock_db.session.add.assert_called_once()
-    #     mock_db.session.commit.assert_called_once()
-    #     self.assertEqual(user.first_name, 'Bob')
-    #     self.assertEqual(user.last_name, 'Smith')
-    #     self.assertEqual(user.phone, '5555555555')
-
-
-    @patch('stocks.service.user_crud.db')
-    def test_update_user(self, mock_session):
-        # Create a mock User object
-        mock_user = User(id=1, first_name='John', last_name='Doe', phone='1234567890')
-
-        # Set up the mock session to return the mock User object when queried
-        mock_session.query.return_value.filter_by.return_value.first.return_value = mock_user
-
-        # Call the update_user function with some new values
-        updated_user = update_user(1, first_name='Bob', last_name='Doe', phone='5555555555')
-        mock_session.commit()
-
-        # Assert that the mock session's commit method was called once
-        mock_session.commit.assert_called_once()
-        self.assertIsNotNone(User.query.filter_by(id=1).first())
-        # Assert that the returned User object has the expected values
-        self.assertEqual(updated_user.id, 1)
-        self.assertEqual(updated_user.first_name, 'Bob')
-        self.assertEqual(updated_user.last_name, 'Doe')
-        self.assertEqual(updated_user.phone, '5555555555')
+    #     # Call the update_user function with some new values
+    #     updated_user = update_user(1, first_name='Bob', last_name='Doe', phone='5555555555')
+    #     mock_session.commit()
+    #
+    #     # Assert that the mock session's commit method was called once
+    #     mock_session.commit.assert_called_once()
+    #     self.assertIsNotNone(User.query.filter_by(id=1).first())
+    #     # Assert that the returned User object has the expected values
+    #     self.assertEqual(updated_user.id, 1)
+    #     self.assertEqual(updated_user.first_name, 'Bob')
+    #     self.assertEqual(updated_user.last_name, 'Doe')
+    #     self.assertEqual(updated_user.phone, '5555555555')
 
 
     @patch('stocks.service.user_crud.User.query')
