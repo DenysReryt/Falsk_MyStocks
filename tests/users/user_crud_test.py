e7a1252e50190b28aeb47d795935bbdfaf0a92e5
DenysReryt <denysreryt28@gmail.com>
2023-04-18 15:29:10 +0300

    work with user_crud_test

--- tests/users/user_crud_test.py
+++ tests/users/user_crud_test.py
@@ -60,19 +60,17 @@ class TestUserCrud(unittest.TestCase):
         self.assertEqual(user.phone, '5555555555')
 
     @patch('stocks.service.user_crud.db')
-    def test_update_user(self, mock_db):
+    def test_update_user(self, mock_db, mock_query):
         mock_user = MagicMock()
         mock_user.id = 1
         mock_user.first_name = 'John'
         mock_user.last_name = 'Doe'
         mock_user.phone = '1234567890'
-
-        mock_db.session.query.return_value.filter_by.return_value.first.return_value = mock_user
+        mock_query.filter_by.return_value.first.return_value = mock_user
 
         updated_user = update_user(1, first_name='Bob', last_name='Doe', phone='5555555555')
-        mock_db.session.commit()
-
         mock_db.session.commit.assert_called_once()
+
         self.assertEqual(updated_user.id, 1)
         self.assertEqual(updated_user.first_name, 'Bob')
         self.assertEqual(updated_user.last_name, 'Doe')
